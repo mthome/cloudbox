3feb7a3ed256a07b564b33d1ec26cb3099cbf64b
Michael Thome <michael.thome@west.com>
2020-02-07 13:14:47 -0500

    virtualenv-like solution for talking to different GKE clusters

---
+++ kubernetes/sample.klusters.js
@@ -0,0 +1,15 @@
+{
+    "qadt": {
+	"project": "ce-avoke-tenant-qa-devtest-01",
+	    "clusters": {
+	    "cde": {
+		"cluster": "cde-cluster",
+		    "zone": "us-central1-a"
+		    },
+		"telphony": {
+		    "cluster": "telephony-cluster",
+			"zone": "us-central1-a"
+			}
+	}
+    }
+}
